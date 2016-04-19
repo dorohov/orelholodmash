89c38f794768665babf9170ec1d6ece35d6230f8
caezar47 <iss47@ya.ru>
2016-04-19 16:51:01 +0300

    add new pages

--- src/block/site/default/head/.document-ready.js
+++ src/block/site/default/head/.document-ready.js
@@ -3,9 +3,16 @@ var url = window.location.pathname;
 $('.navbar-nav a[href="'+url+'"]').parent().addClass('active');
 $('._fs__nav-item a[href="'+url+'"]').parent().addClass('active');
 $('._ncb__nav a[href="'+url+'"]').parent().addClass('active');
+$('._srcb__nav a[href="'+url+'"]').parent().addClass('active');
 //$('.navbar-block__list a[data-post_id="1"]').parent().addClass('active');
 $('#getModal').click(function(){
 	$('#modal-form-enter').modal('hide');
 	setTimeout(function() {$('#modal-form-reg').modal('show');}, 500)
 	return false;
-})
+});
+
+if (device.mobile() || device.tablet()) {
+	$('.navbar').addClass('navbar-fixed-top');
+} else {
+	$('.navbar').removeClass('navbar-fixed-top');
+}
